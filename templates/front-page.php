2b6d07abfbd0d9d8ef21e06d11ff215c235f0e21
olgababiarz <babiarz.olga@gmail.com>
2026-06-17 00:04:17 +0200

    Budowa szkieletu strony głównej i sekcji hero

---
+++ templates/front-page.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * Template Name: Strona główna
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo wp_get_document_title()?></title>
+    <?php wp_head()?>
+</head>
+<body>
+    <header>
+        <a href=""><img src="" alt="KARO Studio logo"></a>
+        <nav class="navigation">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Portfolio</a></li>
+                <li><a href="">Współpraca</a></li>
+                <li><a href="">O mnie</a></li>
+                <li><a href="">Kontakt</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="hero hero--home">
+            <h2 class="hero__name">Katarzyna Rozbicka</h2>
+            <h1 class="section-title hero__title">Architekt wnętrz</h1>
+            <p class="hero__description">Wnętrza projektowane raz. Na zawsze.</p>
+        </section>
+        <section class="o-mnie"></section>
+        <section class="realizacje"></section>
+        <section class="wspolpraca"></section>
+        <section class="faq faq--home"></section>
+        <section class="kontakt-form"></section>
+    </main>
+
+    <footer></footer>
+<?php wp_footer()?>
+</body>
+</html>
--- templates/page-home.php
+++
@@ -1,5 +0,0 @@
-<?php
-/*
- * Template Name: Strona główna
- */
-?>
