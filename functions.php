afd1639b00bf0c542578476f1f5bcbd3cc708378
olgababiarz <babiarz.olga@gmail.com>
2026-06-18 23:54:43 +0200

    Dodanie zmiennych CSS, reset marginesów i Google Fonts

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 function karo_enqueue_styles(){ 
     wp_enqueue_style('karo-style', get_stylesheet_directory_uri() . '/style.css'); 
     wp_enqueue_style('karo-main-style', get_stylesheet_directory_uri() . '/assets/css/main.css'); 
+    wp_enqueue_style('karo-google-fonts', 'https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300..700;1,300..700&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
 }
 
 add_action('wp_enqueue_scripts', 'karo_enqueue_styles');
